0a5761f1104af550945f75646db3b769a2d0e7c9
sm000117 <75351686+ParkSeonMi@users.noreply.github.com>
2022-07-07 20:14:43 +0900

    3-14 검색

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,38 @@
+from flaskext.markdown import Markdown
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 import config
 
-db: SQLAlchemy=SQLAlchemy()
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+migrate = Migrate()
+
+# db: SQLAlchemy=SQLAlchemy()
 # flask db init 로 데베 초기화
 # flask db upgrade
-migrate=Migrate()
 
 def create_app(): # 애플리케이션 팩토리
     app = Flask(__name__)
     app.config.from_object(config)
 
+    # markdown
+    Markdown(app, extensions=['nl2br', 'fenced_code'])
+
     # ORM
     db.init_app(app)
-    migrate.init_app(app, db)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
     from . import models
 
     # 블루프린트
--- app/models.py
+++ app/models.py
@@ -2,12 +2,31 @@ from app import db
 
 # db.session.add/delete -> commit | Model.query.all/filter/get
 
+# N:N
+question_voter= db.Table(
+    'question_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id',ondelete='CASCADE'), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), primary_key=True)
+)
+
+
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # db.Integer && pk => 자동 생성
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('question_set'))
+    modify_date=db.Column(db.DateTime(), nullable=True)
+    voter=db.relationship('User', secondary=question_voter, backref=db.backref('question_voter_set'))
+
+
+answer_voter=db.Table(
+    'answer_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('answer_id', db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), primary_key=True)
+)
 
 
 class Answer(db.Model):
@@ -18,6 +37,10 @@ class Answer(db.Model):
     question = db.relationship('Question', backref=db.backref('answer_set'))
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user=db.relationship('User', backref=db.backref('answer_set'))
+    modify_date=db.Column(db.DateTime(), nullable=True)
+    voter=db.relationship('User',secondary=answer_voter, backref=db.backref('answer_voter_set'))
 
 
 class User(db.Model):
@@ -25,3 +48,5 @@ class User(db.Model):
     username=db.Column(db.String(150), unique=True, nullable=False)
     password=db.Column(db.String(200), nullable=False)
     email=db.Column(db.String(120), unique=True, nullable=False)
+
+
